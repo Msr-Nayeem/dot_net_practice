d15728b891e103bbcccfcbae96facbce9b227be4
Msr Nayeem <msrnayeem@gmail.com>
2023-01-31 17:01:43 +0600

    custom controller, view, data pass, pass with array, redirect

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            @ViewBag.Title = "Dashboard";
+            ViewBag.List = new string[] { "shahidur ", "Nayeem" };
+            return View();
+        }
+        public ActionResult myProfile()
+        {
+            @ViewBag.Title = "My Profile";
+            ViewBag.name = "Shahidur rahman nayeem";
+            ViewBag.section = "B";
+            ViewBag.id = "18-38037-2";
+            
+            return View();
+        }
+        public ActionResult Setting()
+        {
+            @ViewBag.Title = "Setting";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult LoginSubmit()
+        {
+            ViewBag.Message = "Your login submit page.";
+            // if ok
+            TempData["msg"] = "success";
+            return RedirectToAction("Index", "Dashboard");
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Your sign up page.";
+
+            return View();
+        }
+    }
+}
