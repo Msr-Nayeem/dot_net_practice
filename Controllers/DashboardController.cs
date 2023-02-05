5f91a71d9fc5baa69b2147f2195f92aa21f4b241
Msr Nayeem <msrnayeem@gmail.com>
2023-02-05 14:31:31 +0600

    datapass by model,viewbag

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -9,10 +9,37 @@ namespace WebApplication1.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string CGPA { get; set; }
+        }
         public ActionResult Index()
         {
+            Student single = new Student();
+            
+                single.Id = 38037;
+                single.Name = "Nayeem";
+                single.CGPA = "3.30";
+
+           
+            List<Student> multipleStudent = new List<Student>();  
+            for(int i = 0;i<10; i++)
+            {
+                string name="student"+(i+1);
+                int id = i+1;
+                string cgpa = "3.3"+(i);
+
+                multipleStudent.Add(new Student()
+                {
+                    Id = id, CGPA= cgpa, Name= name
+                }) ;
+            }
             @ViewBag.Title = "Dashboard";
-            ViewBag.List = new string[] { "shahidur ", "Nayeem" };
+            ViewBag.List = single;
+            ViewBag.multipleList = multipleStudent;
             return View();
         }
         public ActionResult myProfile()
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
@@ -45,6 +47,31 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your sign up page.";
 
             return View();
+        }
+       public ActionResult SignUpSubmitted()
+        {
+            return View();
+        }
+        public ActionResult Info() { 
+        Home h = new Home();
+            h.Email = "msrnayeem@gmail.com";
+            h.Password= "password";
+            h.Name= "Nayeem";
+        return View(h);
+        }
+        
+        public ActionResult List() {
+            List<Home> homes = new List<Home>();
+            for(int i = 0; i<10; i++)
+            {
+                Home h = new Home()
+                {
+                    Email = "msr" + (i + 1) + "@gmail.com",
+                Name = "nayeem" + (i + 1)
+                };
+            homes.Add(h);
+            }
+            return View(homes);
         }
     }
 }
